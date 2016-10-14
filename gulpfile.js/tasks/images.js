6718b399b28341d621ee05119ee815334a5000bd
Thomas Mattheussen <thomas.mattheussen@gmail.com>
2016-10-14 15:58:46 +0200

    images refactor

--- gulpfile.js/tasks/images.js
+++ gulpfile.js/tasks/images.js
@@ -1,24 +1,23 @@
-var config      = require('../config')
-if(!config.tasks.images) return
+const config = require('../config');
+if (!config.tasks.images) return;
 
-var browserSync = require('browser-sync')
-var changed     = require('gulp-changed')
-var gulp        = require('gulp')
-var imagemin    = require('gulp-imagemin')
-var path        = require('path')
+const
+    browserSync = require('browser-sync'),
+    changed = require('gulp-changed'),
+    gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    path = require('path'),
+    paths = {
+        src: path.join(config.root.src, config.tasks.images.src, '/**/*.{' + config.tasks.images.extensions + '}'),
+        dest: path.join(config.root.dest, config.tasks.images.dest)
+    },
+    imagesTask = () => {
+        return gulp.src([paths.src, '*!README.md'])
+            .pipe(changed(paths.dest)) // Ignore unchanged files
+            .pipe(imagemin()) // Optimize
+            .pipe(gulp.dest(paths.dest))
+            .pipe(browserSync.stream());
+    };
 
-var paths = {
-  src: path.join(config.root.src, config.tasks.images.src, '/**/*.{' + config.tasks.images.extensions + '}'),
-  dest: path.join(config.root.dest, config.tasks.images.dest)
-}
-
-var imagesTask = function() {
-  return gulp.src([paths.src, , '*!README.md'])
-    .pipe(changed(paths.dest)) // Ignore unchanged files
-    .pipe(imagemin()) // Optimize
-    .pipe(gulp.dest(paths.dest))
-    .pipe(browserSync.stream())
-}
-
-gulp.task('images', imagesTask)
-module.exports = imagesTask
+gulp.task('images', imagesTask);
+module.exports = imagesTask;
