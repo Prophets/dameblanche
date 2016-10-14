1dbec3f54b166b679ba998db797d1fe0b220ceeb
Thomas Mattheussen <thomas.mattheussen@gmail.com>
2016-10-14 15:50:28 +0200

    svgsprite refactor

--- gulpfile.js/tasks/svgSprite.js
+++ gulpfile.js/tasks/svgSprite.js
@@ -1,25 +1,25 @@
-var config      = require('../config')
-if(!config.tasks.svgSprite) return
+const config = require('../config');
+if (!config.tasks.svgSprite) return;
 
-var browserSync = require('browser-sync')
-var gulp        = require('gulp')
-var imagemin    = require('gulp-imagemin')
-var svgstore    = require('gulp-svgstore')
-var path        = require('path')
+const
+    browserSync = require('browser-sync'),
+    gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    svgstore = require('gulp-svgstore'),
+    path = require('path'),
+    svgSpriteTask = function() {
+        const settings = {
+            src: path.join(config.root.src, config.tasks.svgSprite.src, '/*.svg'),
+            dest: path.join(config.root.dest, config.tasks.svgSprite.dest)
+        };
 
-var svgSpriteTask = function() {
+        return gulp.src(settings.src)
+            .pipe(imagemin())
+            .pipe(svgstore())
+            .pipe(gulp.dest(settings.dest))
+            .pipe(browserSync.stream());
+    };
 
-  var settings = {
-    src: path.join(config.root.src, config.tasks.svgSprite.src, '/*.svg'),
-    dest: path.join(config.root.dest, config.tasks.svgSprite.dest)
-  }
 
-  return gulp.src(settings.src)
-    .pipe(imagemin())
-    .pipe(svgstore())
-    .pipe(gulp.dest(settings.dest))
-    .pipe(browserSync.stream())
-}
-
-gulp.task('svgSprite', svgSpriteTask)
-module.exports = svgSpriteTask
+gulp.task('svgSprite', svgSpriteTask);
+module.exports = svgSpriteTask;
