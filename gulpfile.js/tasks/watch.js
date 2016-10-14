70c9dc0152569f06cb0ed975bd1e97a4e2fa2119
Thomas Mattheussen <thomas.mattheussen@gmail.com>
2016-10-14 15:48:57 +0200

    watch refactor

--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -1,22 +1,21 @@
-var config = require('../config')
-var gulp   = require('gulp')
-var path   = require('path')
-var watch  = require('gulp-watch')
+const
+    config = require('../config'),
+    gulp = require('gulp'),
+    path = require('path'),
+    watch = require('gulp-watch'),
+    watchTask = function() {
+        const watchableTasks = ['images', 'svgSprite', 'html', 'css', 'eslint', 'stylelint'];
 
-var watchTask = function() {
-  var watchableTasks = ['images', 'svgSprite', 'html', 'css', 'eslint', 'stylelint']
+        watchableTasks.forEach(function(taskName) {
+            const task = config.tasks[taskName];
+            if (task) {
+                const glob = path.join(config.root.src, task.src, '**/*.{' + task.extensions.join(',') + '}');
+                watch(glob, function() {
+                    require('./' + taskName)();
+                });
+            }
+        });
+    };
 
-  watchableTasks.forEach(function(taskName) {
-    var task = config.tasks[taskName]
-    if(task) {
-      var glob = path.join(config.root.src, task.src, '**/*.{' + task.extensions.join(',') + '}')
-      console.log(taskName, glob);
-      watch(glob, function() {
-       require('./' + taskName)()
-      })
-    }
-  })
-}
-
-gulp.task('watch', ['browserSync'], watchTask)
-module.exports = watchTask
+gulp.task('watch', ['browserSync'], watchTask);
+module.exports = watchTask;
