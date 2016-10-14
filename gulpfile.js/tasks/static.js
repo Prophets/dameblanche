af693de84b0654015bc6128807409616e37b7c53
Thomas Mattheussen <thomas.mattheussen@gmail.com>
2016-10-14 15:54:41 +0200

    static refactor

--- gulpfile.js/tasks/static.js
+++ gulpfile.js/tasks/static.js
@@ -1,21 +1,20 @@
-var config  = require('../config')
-var changed = require('gulp-changed')
-var gulp    = require('gulp')
-var path    = require('path')
+const
+    config = require('../config'),
+    changed = require('gulp-changed'),
+    gulp = require('gulp'),
+    path = require('path'),
+    paths = {
+        src: [
+            path.join(config.root.src, config.tasks.static.src, '/**'),
+            path.join('!' + config.root.src, config.tasks.static.src, '/README.md')
+        ],
+        dest: path.join(config.root.dest, config.tasks.static.dest)
+    },
+    staticTask = () => {
+        return gulp.src(paths.src)
+            .pipe(changed(paths.dest)) // Ignore unchanged files
+            .pipe(gulp.dest(paths.dest));
+    };
 
-var paths = {
-  src: [
-    path.join(config.root.src, config.tasks.static.src, '/**'),
-    path.join('!' + config.root.src, config.tasks.static.src, '/README.md')
-  ],
-  dest: path.join(config.root.dest, config.tasks.static.dest)
-}
-
-var staticTask = function() {
-  return gulp.src(paths.src)
-    .pipe(changed(paths.dest)) // Ignore unchanged files
-    .pipe(gulp.dest(paths.dest))
-}
-
-gulp.task('static', staticTask)
-module.exports = staticTask
+gulp.task('static', staticTask);
+module.exports = staticTask;
