78287d2d8c579700500f7ba0a6496bd0334d23d6
Thomas Mattheussen <thomas.mattheussen@gmail.com>
2017-03-02 11:37:37 +0100

    fix to be able to delete the templates task and still have a correct build

--- gulpfile.js/tasks/production.js
+++ gulpfile.js/tasks/production.js
@@ -7,7 +7,7 @@ const
         global.production = true;
 
         const tasks = getEnabledTasks('production');
-        gulpSequence('clean', tasks.lintTasks, tasks.assetTasks, tasks.codeTasks, config.tasks.production.rev ? 'rev' : false, 'size-report', 'static', cb);
+        gulpSequence('clean', tasks.lintTasks, tasks.assetTasks, tasks.codeTasks, 'static', config.tasks.production.rev ? 'rev' : false, 'size-report', cb);
     };
 
 gulp.task('production', productionTask);
