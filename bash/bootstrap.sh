5453fe562870c721d66ce227a095deadc64e51b4
Samuel Joos <sjoos@prophets.be>
2017-03-17 17:29:08 +0100

    update bootstrap sh

--- bash/bootstrap.sh
+++ bash/bootstrap.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-git clone https://git.prophets.be/playground/lets-disagree-to-agree.git .
-git checkout --track -b origin/13-prefix-commit-messages-with-branch-name
-rm -rf .git;
-git init;
-cp git/hooks/prepare-commit-msg .git/hooks/
-git add -A;
-git commit -m 'initial commit';
+git clone https://git.prophets.be/playground/lets-disagree-to-agree.git . 
+#remove this when merged into master
+git fetch origin
+git checkout -t remotes/origin/13-prefix-commit-messages-with-branch-name
+#end remove
+rm -rf .git 
+git init
+cp git/hooks/prepare-commit-msg .git/hooks/prepare-commit-msg
+rm -rf git
+rm -rf bash
+git add -A 
+git commit -m 'initial commit'
