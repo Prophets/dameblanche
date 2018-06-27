b7c0c1979ed8a5ed60c457923b045db2cd4b9f93
Thomas Mattheussen <thomas.mattheussen@gmail.com>
2018-06-27 10:29:04 +0200

    less noise in output for webpack build

--- gulpfile.js/tasks/webpack/webpackProductionBuildLogger.js
+++ gulpfile.js/tasks/webpack/webpackProductionBuildLogger.js
@@ -5,16 +5,22 @@ const handleErrors = require('../../lib/handleErrors');
 module.exports = (err, stats) => {
     if (err) throw new Error(err);
 
-    let statColor = stats.compilation.warnings.length < 1 ? 'green' : 'yellow';
-
     if (stats.compilation.errors.length > 0) {
         stats.compilation.errors.forEach((error) => {
             handleErrors(error);
-            statColor = 'red';
         });
     } else {
         const compileTime = prettifyTime(stats.endTime - stats.startTime);
-        console.log(colors[statColor](stats));
+
+        console.log(
+            '\n',
+            stats.toString({
+                chunks: false,
+                colors: true
+            }),
+            '\n'
+        );
+
         console.log('Compiled with', colors.cyan('webpack'), 'in', colors.magenta(compileTime));
     }
 };
