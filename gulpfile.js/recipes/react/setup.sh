8bf5db0c8d20e7bae466fd15e4564c5683fb069e
Thomas Mattheussen <thomas.mattheussen@gmail.com>
2019-02-11 11:07:31 +0100

    Remove yarn and update patches and minors

--- gulpfile.js/recipes/react/setup.sh
+++ gulpfile.js/recipes/react/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # install js dependencies for react recipe
-yarn add react react-dom
-yarn add babel-plugin-transform-react-jsx
+npm install react react-dom
+npm install babel-plugin-transform-react-jsx
 
 # replace current config file with react recipe config file.
 cp -fi gulpfile.js/recipes/react/config.js gulpfile.js/config.js
