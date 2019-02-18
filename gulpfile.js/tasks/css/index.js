c954c3c1114be3bb55ab9aeaacffa23ca4be82f9
Thomas Mattheussen <thomas.mattheussen@gmail.com>
2019-02-18 14:51:36 +0100

    Use gulp built-in sourcemaps in favor of gulp-sourcemaps

--- gulpfile.js/tasks/css/index.js
+++ gulpfile.js/tasks/css/index.js
@@ -6,7 +6,6 @@ const gulp = require('gulp');
 const gulpif = require('gulp-if');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 const cssnano = require('gulp-cssnano');
 const path = require('path');
@@ -20,16 +19,14 @@ const cssTask = () => {
         dest: path.join(config.root.dest, config.tasks.css.dest)
     };
 
-    return gulp.src(paths.src)
-        .pipe(gulpif(!isProductionBuild(), sourcemaps.init()))
+    return gulp.src(paths.src, { sourcemaps: !isProductionBuild() })
         .pipe(sass(config.tasks.css.sass))
         .on('error', handleErrors)
         .pipe(autoprefixer())
         .pipe(gulpif(isProductionBuild(), cssnano({
             autoprefixer: false
         })))
-        .pipe(gulpif(!isProductionBuild(), sourcemaps.write()))
-        .pipe(gulp.dest(paths.dest))
+        .pipe(gulp.dest(paths.dest, { sourcemaps: !isProductionBuild() }))
         .pipe(customNotifier({ title: 'CSS compiled' }))
         .pipe(browserSync.stream());
 };
